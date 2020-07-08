e907ca5c0ae36570daf3aa3a2d7027d793f641db
Sushant Dahal <sushant73@me.com>
2020-07-07 22:22:31 -0600

    Updated main

--- app/src/main/java/com/example/remotesense/MainActivity.java
+++ app/src/main/java/com/example/remotesense/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.remotesense;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Runs first when app starts
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //layouut of the main landing page
+
+
+//        Button myButton = findViewById(R.id.button1);
+//        myButton.setOnClickListener {sendMessage()};
+
+    }
+
+    public void sendMessage(android.view.View view){
+        Toast.makeText(this, "Button pressed ", Toast.LENGTH_SHORT).show();
+        TextView txtHello= findViewById(R.id.txt_01);
+        EditText edittext= findViewById(R.id.editTxt);
+        txtHello.setText("Bonjour " + edittext.getText().toString() + "  Welcome to RemoteSense");
+
+    }
+
+    public void onBtnClick(android.view.View view){ // button object and method when clicked
+        TextView txtHello= findViewById(R.id.txt_01);
+        EditText edittext= findViewById(R.id.editTxt);
+        txtHello.setText("Bonjour " + edittext.getText().toString() + "  Welcome to RemoteSense");
     }
 }
